782e7552135d51b566e5538717530b70d92e558a
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-05 15:59:47 -0500

    function returns

--- Mod1/strings.js
+++ Mod1/strings.js
@@ -19,3 +19,26 @@ console.log(thirdCharacter); // will log 'r'
 
 var emptyString = "";
 console.log(emptyString);
+
+// Return Values for Functions, and multiple parameters
+
+// first we will declare a function that takes a string parameter and returns it
+function returnString(string) {
+    return string;
+  }
+  var resultString = returnString('hooray for programming');
+  console.log('resultString:', resultString); // we have added console.log messages to help us identify which variable is which when this code is run in the console
+
+  // next, we will declare a function that takes a string parameter and returns the first letter
+function returnFirstCharacter(string) {
+    return string[0];
+  }
+  var firstCharacter = returnFirstCharacter('coffee');
+  console.log('firstCharacter:',firstCharacter);
+
+  // lastly, we will declare a function that takes a string and a number parameter and returns the character located at the given index within the string
+function returnACharacter(string, index) {
+    return string[index];
+  }
+  var character = returnACharacter('tea', 1);
+  console.log('character:', character);
