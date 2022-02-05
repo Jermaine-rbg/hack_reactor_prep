4224b41b05efeb43246c5d3d9076128539e60550
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-05 17:50:45 -0500

    functions and arrays

--- Mod1/arrays.js
+++ Mod1/arrays.js
@@ -12,3 +12,23 @@ var thirdScore = scores[2];
 
 console.log('firstFruit:', firstFruit);
 console.log('thirdScore:', thirdScore);
+
+// next, we will declare a function that takes in an array and an index, and returns the element located at the inputted index
+function returnAnElement(array, index) {
+    return array[index];
+  }
+  
+  var scores = [98, 85, 91, 78, 82];
+  var position = 3;
+  var positionElement = returnAnElement(scores, position);
+  console.log('positionElement:', positionElement);
+  
+// we will declare a finction that takes no parameter, creates a array, and the newly created array
+
+function createAndReturnAnArray(){
+    var newArray = ['some, elements, 4, 5'];
+    return newArray;
+}
+
+var resultArray = createAndReturnAnArray();
+console.log('resultArray:', resultArray);
