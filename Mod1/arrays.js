43dc907d84c3d67034fc4b9f01e9a06c70f55a0f
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-05 16:24:31 -0500

    created array.js file

---
+++ Mod1/arrays.js
@@ -0,0 +1,14 @@
+// The Array "object" is used to store an ordered list of multiple values in a single variable
+
+// Array indexes, like string indexes, are zero-based: the first element in an array is located at index 0, the second at index 1, and so on
+
+// Arrays are considered "objects" in JavaScript,
+
+var fruits = ['oranges', 'bananas', 'apples'];
+var scores = [98, 85, 91, 78, 82];
+
+var firstFruit = fruits[0];
+var thirdScore = scores[2];
+
+console.log('firstFruit:', firstFruit);
+console.log('thirdScore:', thirdScore);
