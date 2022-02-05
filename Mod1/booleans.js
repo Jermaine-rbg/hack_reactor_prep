722ee161680cbef7763076f06bc9d354402ba7ef
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-04 22:32:33 -0500

    boolean with no parameter that returns true

--- Mod1/booleans.js
+++ Mod1/booleans.js
@@ -7,3 +7,12 @@ console.log(hardWork);
 
 var noJob = false;
 console.log(noJob);
+
+// Now we are going to complete a function that takes no parameters and returns true.
+
+// Add a statement that will cause this function to return true when ran.
+
+function returnTrue() {
+    return true;
+  // returns true
+}
