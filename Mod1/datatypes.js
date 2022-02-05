3dc3d98695b4036370189c7b7c0e5a69d0d25479
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-04 19:53:15 -0500

    setup files are done

---
+++ Mod1/datatypes.js
@@ -0,0 +1,23 @@
+// Variables for strings
+
+// The variable is nameForMe and the value is Jermaine
+
+// Jermaine is a string
+
+//Basic variables are declared using the var or let keywords. You can assign variables and set variables using the = symbol.
+
+let nameForMe = "jermaine"
+
+console.log(nameForMe);
+
+//Functions are the prime mechanism with which you will be manipulating data
+
+// this function has one simple operation: it returns 1
+function returnOne() {
+  return 1;
+}
+
+// this part is similar to what happens when you click 'Run Tests'
+var result = returnOne();
+console.log(result); // should log 1 to the console
+
--- Mod1/index.js
+++
@@ -1,9 +0,0 @@
-// This function has one simple operation : it returns 1
-function returnOne() {
-  return 1;
-}
-
-var result = returnOne;
-console.log(result);
-
-//console.log("Hello World");
