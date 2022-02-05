727d0e52df89e833c20d9cfd226ba5333c82d24d
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-04 22:40:59 -0500

    functions that return a boolean

--- Mod1/booleans.js
+++ Mod1/booleans.js
@@ -8,7 +8,23 @@ console.log(hardWork);
 var noJob = false;
 console.log(noJob);
 
+// two examples of functions that return a boolean type value:
+
+// first we will declare a function that takes no parameters, does nothing, and returns true
+function isTrue() {
+    return true;
+  }
+  var trueResult = isTrue(); // this line "calls" the function and assigns the value returned to trueResult
+  console.log(trueResult); // if you run this code in the console, you can see that trueResult has been set to a value of true
+  
+// next, we will declare a function that takes no parameters, does nothing, and returns false
+function isFalse() {
+    return false;
+  }
+  var falseResult = isFalse(); // this line "calls" the function and assigns the value returned to falseResult
+  console.log(falseResult); // if you run this code in the console, you can see that falseResult has been set to a value of false
 // Now we are going to complete a function that takes no parameters and returns true.
+
 
 // Add a statement that will cause this function to return true when ran.
 
