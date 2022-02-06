379cb46f8a9e8f84eac05e250125218619dffa5b
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-05 21:48:44 -0500

    update

---
+++ Mod1/objects.js
@@ -0,0 +1,11 @@
+// In real life, a car is an object
+
+// A car has properties like weight and color, and methods like start and stop:
+
+// An object is a collection of properties
+
+// A property is an association between a name (key) and a value (some data type)
+
+// A property's value can be any of the types we have gone over, as well as being another object, or even a function
+
+// In the case where the value of a property is a function, we refer to the property as a method
