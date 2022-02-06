84c6322b0e2f25362e86675be6f8224c56af40e6
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-05 22:00:51 -0500

    update

--- Mod1/objects.js
+++ Mod1/objects.js
@@ -9,3 +9,26 @@
 // A property's value can be any of the types we have gone over, as well as being another object, or even a function
 
 // In the case where the value of a property is a function, we refer to the property as a method
+
+// first let us look at an object with boolean values
+
+var booleanObj = {
+    isObject: true,
+    isString: false
+};
+
+// next, let us look at an object with number values
+
+var numbersObj = {
+    count: 4,
+    remaining: 7
+};
+
+// finally, let us look at an object with string values
+
+var stringObj = {
+    name: 'Daisy',
+    currentTitle: 'pupper',
+    futurePosition: 'doggo'
+}
+
