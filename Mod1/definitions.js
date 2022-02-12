ffcf63a6fb2ce6ffbf7686ab0175b3e0b0bf554e
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-11 21:47:14 -0500

    Javascript syntax

---
+++ Mod1/definitions.js
@@ -0,0 +1,24 @@
+// Parameters are variables listed as a part of the function definition.
+
+// Arguments are values passed to the function when it is invoked.
+
+// The JavaScript syntax defines two types of values:
+
+// Fixed values are called Literals.
+
+// Variable values are called Variables.
+
+// Numbers are written with or without decimals:
+
+// Strings are text, written within double or single quotes:
+
+// In a programming language, variables are used to store data values.
+
+// JavaScript uses the keywords var, let and const to declare variables.
+
+// An equal sign is used to assign values to variables.
+
+// JavaScript uses arithmetic operators ( + - * / ) to compute values:
+
+// JavaScript uses an assignment operator ( = ) to assign values to variables:
+
