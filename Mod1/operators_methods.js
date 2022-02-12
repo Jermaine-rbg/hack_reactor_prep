cd2c548f121b823af0e4659c0d41621b33a5a204
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-11 21:57:26 -0500

    operators and methods

---
+++ Mod1/operators_methods.js
@@ -0,0 +1,23 @@
+// What is an Operator?
+
+// An operator is used to perform specific computations or operations on operands
+
+// We have seen a few so far: =,+, -, and typeof
+
+// They can be unary: {operator} {operand} (e.g. typeof "username"), or:
+
+// They can be binary: {operand1} {operator} {operand2} (e.g. 4 + 5)
+
+// What is a Method?
+
+// A method is a function that is a property of an object
+
+// We have seen a few so far: console.log, and Array.isArray()
+
+// A few details about console.log and Expressions as Arguments
+
+// console.log() is a method (a function that is a property of an object)
+
+// When we call console.log() we will typically pass it an argument, or sometimes several arguments, where each argument is separated by commas
+
+// The commas are also operators
