695303cb695944767fa6126763ec858e955e0ef1
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-15 22:42:51 -0500

    definitions

--- Mod1/definitions.js
+++ Mod1/definitions.js
@@ -22,3 +22,35 @@
 
 // JavaScript uses an assignment operator ( = ) to assign values to variables:
 
+// JavaScript variables can hold different data types: numbers, strings, objects and more:
+
+// JavaScript evaluates expressions from left to right. Different sequences can produce different results:
+
+// JavaScript has dynamic types. This means that the same variable can be used to hold different data types:
+
+// JavaScript has only one type of numbers.
+
+// Numbers can be written with, or without decimals:
+
+// Booleans can only have two values: true or false
+
+// Booleans are often used in conditional testing.
+
+// JavaScript arrays are written with square brackets.
+
+// Array items are separated by commas.
+
+// Array indexes are zero-based, which means the first item is [0], second is [1], and so on.
+
+// JavaScript objects are written with curly braces {}
+
+// Object properties are written as name:value pairs, separated by commas.
+
+// You can use the JavaScript typeof operator to find the type of a JavaScript variable.
+
+// The typeof operator returns the type of a variable or an expression:
+
+// In JavaScript, a variable without a value, has the value undefined. The type is also undefined.
+
+// Any variable can be emptied, by setting the value to undefined. The type will also be undefined.
+
