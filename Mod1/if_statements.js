ab162945591f30d5ea9f35253ae6bdf9a44956db
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-19 19:15:40 -0500

    if statement

---
+++ Mod1/if_statements.js
@@ -0,0 +1,15 @@
+// What is an IF statement?
+// if statement with undefined
+
+var declared;
+
+if (declared === undefined) {
+  console.log('declared is undefined');
+}
+
+// if statement with numbers
+
+var declared;
+if (declared === undefined){
+    console.log('declared');
+}
