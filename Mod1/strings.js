eada3f85d1288258d7e341b560bd711936b5440f
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-23 17:21:34 -0500

    sub strings

--- Mod1/strings.js
+++ Mod1/strings.js
@@ -187,3 +187,13 @@ function applyIndexOf(string, subString) {
   return index;
 }
 
+// We are going to complete a function that takes in one parameter, and returns a string version of that parameter. Your function should create a stringVersion variable and assign it to a call of the toString() method on the input parameter, and return the stringVersion variable. 
+
+function applyToString(param) {
+  // create a stringVersion variable
+  // assign it to the string version of the input param
+  var stringVersion = param.toString();
+  // return the stringVersion variable
+  return stringVersion;
+}
+
