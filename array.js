93923e8f2aa62d5263657fce0f2a13748f3a2b89
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-02-24 23:01:34 -0500

    arrays

---
+++ array.js
@@ -0,0 +1,5 @@
+// The Array "object" is used to store an ordered list of multiple values in a single variable
+
+// The values of an Array can be of any type
+
+// Array indexes, like string indexes, are zero-based: the first element in an array is located at index 0, the second at index 1, and so on
