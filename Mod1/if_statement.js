be601f0298b7edcd27135fd4cf84359823cff86f
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-03-04 23:45:27 -0500

    if statemnt

---
+++ Mod1/if_statement.js
@@ -0,0 +1,1 @@
+// A conditional that allows your program to do something, if some condition(1) is true, and do something else, if that same condition(1) is false and another condition(2) is true, and lastly, to do a third action if both condition(1) and condition(2) are both false
