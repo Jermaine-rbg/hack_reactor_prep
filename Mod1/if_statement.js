0d9a9ce32a22ea677df5b6aa5e6a031829d11276
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-03-04 23:46:53 -0500

    if statemnt

--- Mod1/if_statement.js
+++ Mod1/if_statement.js
@@ -1,1 +1,21 @@
 // A conditional that allows your program to do something, if some condition(1) is true, and do something else, if that same condition(1) is false and another condition(2) is true, and lastly, to do a third action if both condition(1) and condition(2) are both false
+
+
+// if (/*some expression which evaluates to true or false*/ {
+
+    /*some action to be taken in the event
+    that the expression above evaluates to true*/
+  
+  // else if (/*some other expression which evaluates to true or false*/ {
+  
+    /*some action to be taken in the event
+    that the first expressions evaluates to false
+    and the second expression above evaluates to true*/
+  
+  // } else {
+  
+    /*some action to be taken in the event
+    that the first expressions evaluates to false
+    and the second expression above evaluates to false*/
+  
+  // }
