5ed6b32e982c5724a81e72f04d9d43c6a57c1d45
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-03-05 22:54:09 -0500

    conditionals

---
+++ Mod1/conditionals.js
@@ -0,0 +1,14 @@
+// What is Pseudocode?
+// We have seen it before
+// English language statements that describe what the code will do
+// Should be structured similarly to the way the eventual code will be (indentation, etc.)
+// Each line of pseudocode should correspond to one line of actual code
+
+// A conditional that allows your program to do something, if some condition is true
+// Armed with an if statement, this is easily translatable into code:
+
+var rollinSixOwe = true;
+
+if (rollinSixOwe === true) {
+  console.log("you cripping");
+}
