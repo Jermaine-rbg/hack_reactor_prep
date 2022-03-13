1bf800667e2cb589550b5a5e07fb0203afec731f
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-03-12 23:00:52 -0500

    loops

---
+++ Mod1/functions.js
@@ -0,0 +1,16 @@
+// Functions are one of the fundamental building blocks in JS
+// A function is a JavaScript procedure, which is:
+// A set of statements that perform a task or calculate a value
+// A function definition (or declaration) consists of a function keyword, followed by:
+// The name of the function
+// A list of parameters to the function, enclosed in parentheses and separated by commas
+// The JS statements that define the function, enclosed in curly braces 
+
+// definition
+function nameOfFunction(/*list of parameters*/) {
+    /*statements that define the function*/
+  }
+  
+  //calling the function (test)
+  var result = nameOfFunction(/*list of arguments*/);
+  console.log(result);
