8c9c11a562f89af248be629682248d409419db6b
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-06-20 17:35:38 -0400

    digitalcrafts

--- Mod1/digitalcrafts.js
+++ Mod1/digitalcrafts.js
@@ -12,3 +12,9 @@ const firstNumber = 60;
 const lastNumber = 60;
 const nhood = firstNumber + lastNumber;
 console.log(nhood);
+console.log("Rolling", nhood, "Crips")
+
+// Up to now, we have passed the console.log() function multiple values, separated by commas. If we wanted to do something more complicated with the output, we might need to use the string interpolation syntax.
+
+const theAnswer = 30 + 30;
+console.log(`The answer is ${theAnswer} Isn't that great`);
