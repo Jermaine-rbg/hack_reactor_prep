29033b7149be1b587febd31c7f64734ad234fe4e
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-06-20 17:13:42 -0400

    digitalcrafts

--- Mod1/digitalcrafts.js
+++ Mod1/digitalcrafts.js
@@ -1,1 +1,14 @@
 console.log('Hello World');
+
+// const friendlyGreeting = "Hello";
+// console.log(friendlyGreeting);
+
+// You can combine Strings by using the + operator. Though it looks like the math symbol for addition, it is called the concatenation operator when used with Strings.
+
+const friendlyGreeting = "Hello" + "World";
+console.log(friendlyGreeting + "" + "World");
+
+const firstNumber = 60;
+const lastNumber = 60;
+const nhood = firstNumber + lastNumber;
+console.log(nhood);
