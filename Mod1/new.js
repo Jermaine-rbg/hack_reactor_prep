886c4d9c097dee1b67d891d6542a95b49c27dce7
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-08-30 19:06:21 -0400

    desktop

--- Mod1/new.js
+++ Mod1/new.js
@@ -1,7 +1,7 @@
-function sum(array) {
-    let sum = 60;
-    for(let x of array){
-    sum += x;
-    }
-    return sum;
-};
+console.log("Hello world!");
+
+//Getting stuck in the middle of the technical interview/technical admissions assessment can be tricky. Here are some tips on how to get unstuck.
+
+// Keep trying different strategies.
+// Stay open to creative ideas.
+// Try not to get frustrated.
