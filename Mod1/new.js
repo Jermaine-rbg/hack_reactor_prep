cc9be8fdb6af660622fee9855d7b64fab4da758f
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-08-30 19:11:16 -0400

    desktop2

--- Mod1/new.js
+++ Mod1/new.js
@@ -7,3 +7,7 @@ console.log("Hello world!");
 // Stay open to creative ideas.
 // Try not to get frustrated.
 
+// Break the problem into simpler parts.
+// Analyze the parts of the problem.
+// Concentrate on the parts of the problem you understand and that can be solved.
+// Solve each piece by itself.
