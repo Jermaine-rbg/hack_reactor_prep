05c03841d8befe5a760a16ba528485d685667ce5
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-08-30 20:33:59 -0400

    desktop5

--- Mod1/new.js
+++ Mod1/new.js
@@ -19,4 +19,12 @@ console.log("Getting unstuck")
 // what you have done
 // where you are stuck
 
+// Re-describe the problem.
+// Think of simpler ways to describe the problem
 
+// This is sometimes called "rubber-ducking" because there is an anecdote about an engineer who would keep a rubber duck (like what little kids use in the bathtub at his desk. When he got stuck, he would talk it over with his duck.
+
+// Look for hidden assumptions or information you have forgotten to use.
+// After reading each phrase or sentence of the problem statement, ask yourself if any assumptions can be inferred from that phrase.
+// Check again to make sure you are solving the right problem.
+// Double-check all of your values, assumptions, and approaches. Make sure you haven't missed anything and that you are looking for the correct solution.
