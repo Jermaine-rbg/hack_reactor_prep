9e33e80366b0e9e5466c336ed3cf24d9e29b7cda
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-08-31 23:07:51 -0400

    checklist

---
+++ Mod1/checklist.js
@@ -0,0 +1,17 @@
+// Below is a list of necessary technical skills in which you should be proficient by the time of the interview/admissions assessment.
+
+// Basic Level, the student can:
+// Declare a variable
+// Write a function definition
+// Write a function that accepts arguments
+// Write a function that returns a value
+// Write a function that returns a value based on passed in arguments
+// Use an ordinary if /else if /else / statements
+// Use math operators
+// Use logical operators
+// Create a string
+// Concatenate strings
+// Create an array
+// Create an object
+// Create and use a basic for loop and a for-in loop
+// Create and use a while loop
--- Mod1/new.js
+++ Mod1/new.js
@@ -1,54 +1,15 @@
+function sum (a,b) {
+    
+    let result = a + b;
+    return result;
 
-console.log("Getting unstuck")
+}
 
-//Getting stuck in the middle of the technical interview/technical admissions assessment can be tricky. Here are some tips on how to get unstuck.
+ let sumTotal = sum(6, 0);
 
-// Keep trying different strategies.
-// Stay open to creative ideas.
-// Try not to get frustrated.
+ function printName (name) {
+    console.log('Hi' + name)
+ }
 
-// Break the problem into simpler parts.
-// Analyze the parts of the problem.
-// Concentrate on the parts of the problem you understand and that can be solved.
-// Solve each piece by itself.
+ let result = printName("Jon")
 
-// Rubber ducking
-// Verbalize the problem to yourself and others.
-
-// what the problem is
-// what you have done
-// where you are stuck
-
-// Re-describe the problem.
-// Think of simpler ways to describe the problem
-
-// This is sometimes called "rubber-ducking" because there is an anecdote about an engineer who would keep a rubber duck (like what little kids use in the bathtub at his desk. When he got stuck, he would talk it over with his duck.
-
-// Look for hidden assumptions or information you have forgotten to use.
-// After reading each phrase or sentence of the problem statement, ask yourself if any assumptions can be inferred from that phrase.
-// Check again to make sure you are solving the right problem.
-// Double-check all of your values, assumptions, and approaches. Make sure you haven't missed anything and that you are looking for the correct solution.
-
-// Re-evaluate your strategy
-// Try using a different strategy. There is usually more than one way to solve a problem, and you may find a method that you haven't considered is much easier than the one you're working on currently.
-
-// Consider a different core data structure.
-// Consider a different algorithm a different way of processing the data
-// Consider using different helper functions different built-in JavaScript methods, say
-
-// Be the interpreter
-// Walk through your code as though you were the interpreter.
-// To "be the interpreter" means to evaluate every small step in a section of code. And by "evaluating", we mean actually evaluating the code all the way down to the literal value which are the end result of evaluation.
-// console.log statements can help as well, but make sure to use them precisely.
-// Then, when you see the concrete output, it's a lot easier to realize what is wrong.
-
-
-// JavaScript Data Types
-// String	"Hello World"
-// Number	42
-// Boolean	true
-// Date	new Date(10, 21, 2015)
-// Null	null
-// Array	[1,2,3]
-// Object	{ name: "Sarah" }
-// Function	function greetUser(){}
