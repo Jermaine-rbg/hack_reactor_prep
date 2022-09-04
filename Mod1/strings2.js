4f207bd09db42f493a2b65f40a12a70d3decfe72
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-09-03 22:46:50 -0400

    strings2

--- Mod1/strings2.js
+++ Mod1/strings2.js
@@ -10,3 +10,21 @@ function returnString(string) {
 
 let resultString = returnString('richRollin')
 console.log(resultString);
+
+// function hackReactor(strings) {
+//     return strings;
+    
+// }
+
+
+// let prepMod = hackReactor('multiple params');
+// console.log('prepMod:', prepMod); // we have added console.log messages to help us identify which variable is which when this code is run in the console
+
+// next, we will declare a function that takes a string parameter and returns the first letter
+function returnFirstCharacter(string) {
+    return string[0];
+    
+}
+
+let firstCharacter = returnFirstCharacter('prepMod')
+console.log('firstCharacter:', firstCharacter);
