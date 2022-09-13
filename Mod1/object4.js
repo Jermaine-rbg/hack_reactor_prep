be66758d4ec2e3c2ce3c9778f92099779ef3ee69
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-09-12 23:21:27 -0400

    functions3

--- Mod1/object4.js
+++ Mod1/object4.js
@@ -15,3 +15,14 @@ function addProperty(obj, key){
 function removeProperty(obj, key){
     delete obj[key];
 }
+
+// Write a function called "getFullName". Given a first and a last name, "getFullName" returns a single string with the given first and last names separated by a single space.
+
+function getFullName(firstName, lastName){
+
+}
+
+function getFullName(firstName, lastName){
+    let result = firstName + '' + lastName;
+    return result;
+}
